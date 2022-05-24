2b244b28c9dd88de8747eeb4ffcf8481897d16e7
Leonardo Martins <leomartins2001@gmail.com>
2022-05-24 00:25:59 -0300

    :rocket: Posting finished project

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.mobile-menu').addEventListener('click', () =>{
+    document.querySelector('.nav-list').classList.toggle('active')
+    document.querySelector('.mobile-menu').classList.toggle('active')
+})
